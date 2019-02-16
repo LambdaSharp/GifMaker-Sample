49ff60876b69e075ebd29691ce198874e47fae8b
Steve G. Bjorg <steve.bjorg@gmail.com>
2019-02-15 16:49:31 -0800

    initial commit

---
+++ MakeGifFunction/Function.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Amazon.S3.Model;
+using LambdaSharp;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace LambdaSharp.Demo.GifMaker.MakeGifFunction {
+
+    public class Function : ALambdaFunction<S3Event, string> {
+
+        //--- Fields ---
+        private IAmazonS3 _s3Client;
+        private string _destinationBucketName;
+
+        //--- Methods ---
+        public override async Task InitializeAsync(LambdaConfig config) {
+            _s3Client = new AmazonS3Client();
+
+            // show contents of /opt folder; the 'ffmpeg' file must have execution permissions to be invocable
+            LogInfo(Exec("/bin/bash", "-c \"ls -al /opt\"").Output);
+
+            // read configuration parameters
+            _destinationBucketName = config.ReadS3BucketName("AnimatedGifBucket");
+        }
+
+        public override async Task<string> ProcessMessageAsync(S3Event request, ILambdaContext context) {
+            LogInfo($"received {request.Records.Count} records");
+
+            // process records
+            foreach(var record in request.Records) {
+                string originalFilePath = null;
+                string convertedFilePath = null;
+                try {
+
+                    // download file to temporary storage
+                    LogInfo($"downloading 's3://{record.S3.Bucket.Name}/{record.S3.Object.Key}'");
+                    var response = await _s3Client.GetObjectAsync(new GetObjectRequest {
+                        BucketName = record.S3.Bucket.Name,
+                        Key = record.S3.Object.Key
+                    });
+                    originalFilePath = $"/tmp/{Path.GetFileName(record.S3.Object.Key)}";
+                    await response.WriteResponseStreamToFileAsync(
+                        filePath: originalFilePath,
+                        append: false,
+                        cancellationToken: CancellationToken.None
+                    );
+
+                    // run ffmpeg process to convert file to gif
+                    LogInfo("invoking ffmpeg converter");
+                    convertedFilePath = Path.ChangeExtension(originalFilePath, ".gif");
+                    var outcome = Exec("/opt/ffmpeg", $"-i \"{originalFilePath}\" -f gif \"{convertedFilePath}\"");
+                    if(outcome.ExitCode != 0) {
+                        LogWarn(
+                            "FFmpeg exit code: {0}\nError Output: {1}\nStandard Output: {2}",
+                            outcome.ExitCode,
+                            outcome.Error,
+                            outcome.Output
+                        );
+                        continue;
+                    }
+
+                    // upload converted file
+                    var targetKey = Path.Combine(Path.GetDirectoryName(record.S3.Object.Key), Path.GetFileName(convertedFilePath));
+                    LogInfo($"uploading 's3://{_destinationBucketName}/{targetKey}'");
+                    await _s3Client.PutObjectAsync(new PutObjectRequest {
+                        BucketName = _destinationBucketName,
+                        Key = targetKey,
+                        FilePath = convertedFilePath
+                    });
+                } catch(Exception e) {
+
+                    // log the error and continue to the next item
+                    LogError(e);
+                } finally {
+
+                    // delete the original and converted files
+                    DeleteFile(originalFilePath);
+                    DeleteFile(convertedFilePath);
+                }
+            }
+            return "Ok";
+        }
+
+        private (int ExitCode, string Output, string Error) Exec(string application, string arguments) {
+            LogInfo($"executing: {application} {arguments}");
+            using(var process = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = application,
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                },
+                EnableRaisingEvents = true
+            }) {
+                process.Start();
+                var output = Task.Run(() => process.StandardOutput.ReadToEndAsync());
+                var error = Task.Run(() => process.StandardError.ReadToEndAsync());
+                process.WaitForExit();
+                return (ExitCode: process.ExitCode, Output: output.Result, Error: error.Result);
+            }
+        }
+
+        private void DeleteFile(string filePath) {
+            if(filePath == null) {
+                return;
+            }
+            try {
+                File.Delete(filePath);
+            } catch {
+                LogWarn("unable to delete: {0}", filePath);
+            }
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,62 @@
+# λ# - Create Animated GIFs from Videos with AWS Lambda
+
+[This sample requires the λ# tool to deploy.](https://github.com/LambdaSharp/LambdaSharpTool)
+
+## Overview
+
+This λ# module creates a Lambda function that converts videos to animated GIFs. The conversion is done by the [FFmpeg application](https://www.ffmpeg.org/) which is deployed as a [Lambda Layer](https://docs.aws.amazon.com/lambda/latest/dg/configuration-layers.html). The module uses two S3 buckets: one for uploading videos and one for storing the converted animated GIFs. The Lambda function is automatically invoked when a file is uploaded the video S3 bucket.
+
+## Deploy
+
+This module is compiled to CloudFormation and deployed using the λ# CLI.
+
+```
+git clone https://github.com/LambdaSharp/GifMaker-Sample.git
+cd GifMaker-Sample
+lash deploy
+```
+
+## Details
+
+1. Compress contents of `Assets/FFmpeg-Layer` folder into a zip package with  execution permissions for `ffmpeg` executable.
+1. Create a Lambda layer using the uploaded package.
+1. Create Lambda function
+1. (Optional) Create an S3 bucket for uploading videos.
+1. (Optional) Create an S3 bucker for storing the converted animated GIFs.
+
+
+## Parameters
+
+<dl>
+
+<dt><code>VideoBucket</code></dt>
+<dd>
+The <code>VideoBucket</code> parameter can optionally be provided to specify the ARN of an existing S3 bucket where video will be uploaded. A new S3 bucket is automatically created when this parameter is omitted.
+
+<i>Required</i>: No
+
+<i>Type</i>: String (S3 Bucket ARN)
+</dd>
+
+<dt><code>AnimatedGifBucket</code></dt>
+<dd>
+The <code>AnimatedGifBucket</code> parameter can optionally be provided to specify the ARN of an existing S3 bucket where the converted animated GIF files are uploaded to. A new S3 bucket is automatically created when this parameter is omitted.
+
+<i>Required</i>: No
+
+<i>Type</i>: String (S3 Bucket ARN)
+</dd>
+
+</dl>
+
+
+## Attribution
+
+The FFmepg application is provided by https://www.ffmpeg.org/
+
+
+## License
+
+_Apache 2.0_ for the module and code.
+
+_GPL 3.0_ for the files in the `Assets/FFmpeg-Layer` folder.
